d7e01caa9c380a3e149d022471154c9239543f8b
om <disky@oms-MacBook-Pro.local>
2020-05-25 13:31:49 +0300

    changed name

--- example.go
+++ example.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	conf "github.com/d15ky/beebot/config"
-	"github.com/d15ky/beebot/internal/utils"
-	cluster "github.com/d15ky/beebot/internal/cluster"
-	database "github.com/d15ky/beebot/internal/db"
-	"github.com/d15ky/beebot/internal/slackbot"
+	conf "github.com/d15ky/git-hooks-testing/config"
+	"github.com/d15ky/git-hooks-testing/internal/utils"
+	cluster "github.com/d15ky/git-hooks-testing/internal/cluster"
+	database "github.com/d15ky/git-hooks-testing/internal/db"
+	"github.com/d15ky/git-hooks-testing/internal/slackbot"
 	"github.com/gorilla/mux"
 	"net/http"
 	"log"
